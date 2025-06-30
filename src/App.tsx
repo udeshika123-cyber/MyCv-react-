7ba077e0a9692df87a794af318ad1d09f10d66e3
udeshika123cyber <udeshikaprabhashini21@gmail.com>
2025-06-30 16:36:03 +0530

    modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,89 @@
-import React from 'react'
-import './App.css'
-import MySelf from './components/mySelf.tsx'
-import Contact from './components/Contact.tsx'
-import Education from './components/Education.tsx'
-import Reference from './components/Reference.tsx'
-import myPhoto from './assets/myPhoto.jpg'
+import React from "react";
+import "./App.css";
+import MySelf from "./components/MySelf.tsx";
+import Contact from "./components/Contact.tsx";
+import Education from "./components/Education.tsx";
+import Reference from "./components/Reference.tsx";
+import myPhoto from "./assets/myPhoto.jpg";
 
 const App = () => {
   return (
     <>
-    <img src={myPhoto} alt="My Photo" style={{width:'100px',height:'100px'}}/>
-      <div className='head'><b>Udeshika Dayananda </b><br/></div><div className='h1'><b>University Student<br/>===============================================</b> </div><br/>
-      <div className='myDetails'>
-      <MySelf country="Sri Lanka" name='K.A.Udeshika Prabhashini' university="SLTC Research University" NicNo="20027030219" gender="Female"address="Hapugahakumbura,Balangoda" hobbies='Reading novels,Watching movies'status='Unmarried'Date_of_birth='2002/07/21'/>
+      <img
+        src={myPhoto}
+        alt="My Photo"
+        style={{ width: "100px", height: "100px" }}
+      />
+      <div className="head">
+        <b>Udeshika Dayananda </b>
+        <br />
+      </div>
+      <div className="h1">
+        <b>
+          University Student
+          <br />
+          ===============================================
+        </b>{" "}
+      </div>
+      <br />
+      <div className="myDetails">
+        <MySelf
+          country="Sri Lanka"
+          name="K.A.Udeshika Prabhashini"
+          university="SLTC Research University"
+          NicNo="20027030219"
+          gender="Female"
+          address="Hapugahakumbura,Balangoda"
+          hobbies="Reading novels,Watching movies"
+          status="Unmarried"
+          Date_of_birth="2002/07/21"
+        />
       </div>
       <h1>
-        <div className='Contact'><u>Contact</u></div>
+        <div className="Contact">
+          <u>Contact</u>
+        </div>
       </h1>
-      <Contact Phone_Number={+94771234567} Email="udeshikaprabhashini21@gmail.com" LinkedIn="https://www.linkedin.com/in/udeshika-dayananda-1b0a5a1b6/" GitHub="udeshika123-cyber" Facebook="https://www.facebook.com/udeshika.prabhashini.1" Address="Hapugahakumbura,Balangoda"/>
+      <Contact
+        Phone_Number={+94771234567}
+        Email="udeshikaprabhashini21@gmail.com"
+        LinkedIn="https://www.linkedin.com/in/udeshika-dayananda-1b0a5a1b6/"
+        GitHub="udeshika123-cyber"
+        Facebook="https://www.facebook.com/udeshika.prabhashini.1"
+        Address="Hapugahakumbura,Balangoda"
+      />
 
       <h1>
-        <div className='title'><u>Education</u></div></h1>
-        
-        <Education examName1=" - 2021(2022)" examName2="- 2018" examName3="- BSc in Information Technology"/>
+        <div className="title">
+          <u>Education</u>
+        </div>
+      </h1>
+
+      <Education
+        examName1=" - 2021(2022)"
+        examName2="- 2018"
+        examName3="- BSc in Information Technology"
+      />
       <h1>
-        <div className='Reference2'>
+        <div className="Reference2">
           <u>References</u>
         </div>
       </h1>
-      <Reference name='Dr.Mohomed Azmeer'
-      phone= '+94777348691' email='azmeerm@sltc.ac.lk'company='SLTC Research University'/>
-      <br/>
-      <Reference name='Dilanka Galagama'
-      phone= '+94716351865' email='chathu@123gmail.com'company='IT Instructor-National Youth Corps'/>
+      <Reference
+        name="Dr.Mohomed Azmeer"
+        phone="+94777348691"
+        email="azmeerm@sltc.ac.lk"
+        company="SLTC Research University"
+      />
+      <br />
+      <Reference
+        name="Dilanka Galagama"
+        phone="+94716351865"
+        email="chathu@123gmail.com"
+        company="IT Instructor-National Youth Corps"
+      />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/MySelf.tsx
+++ src/components/MySelf.tsx
@@ -1,29 +1,59 @@
-import React from 'react'
-import './mySelf.css'
+import React from "react";
+import "./mySelf.css";
 
 interface MySelfProps {
-    name: string;
-    university: string;
-    NicNo :string;
-    gender:string;
-    country: string; 
-    address:string
-    hobbies:string
-    status:string
-    Date_of_birth :string
+  name: string;
+  university: string;
+  NicNo: string;
+  gender: string;
+  country: string;
+  address: string;
+  hobbies: string;
+  status: string;
+  Date_of_birth: string;
 }
 
-const mySelf = ({name, university, NicNo, gender, country, address, hobbies, status, Date_of_birth}: MySelfProps) => {
+const mySelf = ({
+  name,
+  university,
+  NicNo,
+  gender,
+  country,
+  address,
+  hobbies,
+  status,
+  Date_of_birth,
+}: MySelfProps) => {
   return (
     <>
-    <div className='mySelf-header'>
-        <h1><u>Personal Profile</u></h1></div>
-    <div className='mySelf'>
-        
-    <h2>Name In Full : {name}<br/>University : {university}<br/>NIC NO :{NicNo}<br/>Gender : {gender}<br/>Country: {country}<br/>Address :  {address}<br/>Hobbies :{hobbies}<br/>Status : {status}<br/>Date_of_birth : {Date_of_birth}</h2>
-    </div>
+      <div className="mySelf-header">
+        <h1>
+          <u>Personal Profile</u>
+        </h1>
+      </div>
+      <div className="mySelf">
+        <h2>
+          Name In Full : {name}
+          <br />
+          University : {university}
+          <br />
+          NIC NO :{NicNo}
+          <br />
+          Gender : {gender}
+          <br />
+          Country: {country}
+          <br />
+          Address : {address}
+          <br />
+          Hobbies :{hobbies}
+          <br />
+          Status : {status}
+          <br />
+          DOB : {Date_of_birth}
+        </h2>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default mySelf
+export default mySelf;
